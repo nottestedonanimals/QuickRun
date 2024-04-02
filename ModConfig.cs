f91145b26f9d75c6cf46604657730b68107c276f
nottestedonanimals <nottestedonanimals207@gmail.com>
2024-04-02 19:39:23 -0400

    reduce default speed buff to be closer to 1.5 mod performance

--- ModConfig.cs
+++ ModConfig.cs
@@ -3,7 +3,7 @@
 namespace QuickRun {
     class ModConfig {
 
-        public int SpeedBoost { get; set; } = 10;
+        public int SpeedBoost { get; set; } = 5;
         public KeybindList SprintKey { get; set; } = KeybindList.Parse("Space, LeftStick");
 
     }

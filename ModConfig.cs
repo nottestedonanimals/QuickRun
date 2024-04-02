cb84192be245c1fc300d4fd3d2b6a5083ed2fafc
nottestedonanimals <nottestedonanimals207@gmail.com>
2024-04-02 19:24:07 -0400

    remove commented code

--- ModConfig.cs
+++ ModConfig.cs
@@ -1,9 +1,4 @@
 ﻿using StardewModdingAPI.Utilities;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 
 namespace QuickRun {
     class ModConfig {
